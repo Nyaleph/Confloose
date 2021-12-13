5ee01d05c445c125d54a9242e00d40878c4cc1e7
Nyaleph <yael.magnier@epita.fr>
2021-12-13 15:42:51 +0100

    Updated colorize to check automatically for screen

--- colorize.sh
+++ colorize.sh
@@ -1,10 +1,12 @@
+screen_name=$(xrandr --current | grep primary | cut -f 1 -d " ")
+
 function randColor() {
 
 r=$(($RANDOM % 9 + 1))
 g=$(($RANDOM % 9 + 1))
 b=$(($RANDOM % 9 + 1))
 
-xrandr --output eDP-1 --gamma $r:$g:$b
+xrandr --output $screen_name --gamma $r:$g:$b
 
 }
 
